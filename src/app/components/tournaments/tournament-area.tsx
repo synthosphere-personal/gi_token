208ee8e41b24384e113acdc0e4508025d03f2393
synthosphere-tech <154661462+synthosphere-tech@users.noreply.github.com>
2024-09-26 14:33:25 +0530

    Update tournament-area.tsx

--- src/app/components/tournaments/tournament-area.tsx
+++ src/app/components/tournaments/tournament-area.tsx
@@ -53,8 +53,6 @@ const TournamentArea = () => {
                   </div>
                   <div className="about__content-two">
                     <p>
-                    <span className="step" style={{color: "#FFBE18", fontWeight: "bold", fontSize: "24px"}}>Token distribution will be done within 24 hours: 100%<br /></span>
-                    <span className="step" style={{color: "#FFBE18", fontWeight: "bold", fontSize: "24px" }}>Token distribution will be both : 50:50 <br />BSC and Polygon Network<br /></span>
                     <span className="step" style={{color: "#0D6EFD", fontWeight: "bold", fontSize: "32px" }}>Presale Coming Soon!!! <br />Presale Price - $0.0038</span>
                     </p>
                   </div>
